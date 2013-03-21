b9e89fc4b117b96497191d598e557fae072f8a96
sstratos <sstratos@3e18062a-d356-4dbc-89d4-0d1ceabca008>
2013-03-21 20:09:49 +0000

    removed glib timer tests from MA plugin

--- plugins/MA/MAObject.cpp
+++ plugins/MA/MAObject.cpp
@@ -23,7 +23,7 @@
 #include "ta_libc.h"
 #include "MADialog.h"
 #include "Util.h"
-#include <glib.h>
+//#include <glib.h>
 
 #include <QtDebug>
 
@@ -135,7 +135,7 @@ MAObject::update (ObjectCommand *oc)
   QMap<int, Data *> data = toc.map();
 
 
-  GTimer *timer = g_timer_new();
+//  GTimer *timer = g_timer_new();
   
   int size = data.size();
   TA_Real input[size];
@@ -178,11 +178,11 @@ MAObject::update (ObjectCommand *oc)
     _bars.insert(it.key(), b);
   }
 
-  g_timer_stop(timer);
-  gulong ms = 0;
-  gdouble seconds = g_timer_elapsed(timer, &ms);
-  g_print("MAObject::update: time elapsed %g\n", seconds);
-  g_timer_destroy(timer);
+//  g_timer_stop(timer);
+//  gulong ms = 0;
+//  gdouble seconds = g_timer_elapsed(timer, &ms);
+//  g_print("MAObject::update: time elapsed %g\n", seconds);
+//  g_timer_destroy(timer);
   
   return 1;
 }
